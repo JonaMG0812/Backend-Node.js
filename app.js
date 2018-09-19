f4ababb537aff890abd7c4102261363bcfdf09d7
Jonathan IMG <join_mg@hotmail.com>
2018-09-19 13:24:09 -0500

    app.js update
    Header s and CORS config

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-'use stritc'
+'use strict'
 
 var express = require('express');
-var bodyParser = require ('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 //Loading routes files
-var project_routes =  require('./routes/project');
+var project_routes = require('./routes/project');
 
 //Middlewares
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//CORS
+//Headers and CORS config
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Loading routes
 app.use('/api', project_routes);
