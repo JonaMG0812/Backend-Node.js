d8c7f9a67f756ab660133bea24fb96853e8bc6cc
Jonathan IMG <join_mg@hotmail.com>
2018-09-19 13:37:18 -0500

    .

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var port = 3700;
 //Connecting to the database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/portfolio', { useNewUrlParser: true })
-    .then(() => {
-        console.log('Connection to the database successfully established');
-        //Creating server
-        app.listen(port, () => {
-            console.log("Running server correctly in: localhost:3700");
-        });
-    })
-    .catch(error => console.log(error));
+	.then(() => {
+		console.log('Connection to the database successfully established');
+		//Creating server
+		app.listen(port, () => {
+			console.log("Running server correctly in: localhost:3700");
+		});
+	})
+	.catch(err => console.log(err));
